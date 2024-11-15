62ba63cd430736abd2d9d71cbe64dff2997e7920
joaoschefer <joaogfschefer2121@gmail.com>
2024-11-15 13:51:09 -0300

    outro commit

---
+++ loja_carros/carros/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = 'carros'
+
+urlpatterns = [
+    path('', views.listar_carros, name='listar_carros'),
+    path('<int:carro_id>/', views.detalhes_carro, name='detalhes_carro'),
+    path('cadastrar/', views.cadastrar_carro, name='cadastrar_carro'),
+]
--- loja_carros/carros/views.py
+++ loja_carros/carros/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Carro
+from .forms import CarroForm
 
-# Create your views here.
+# view que vai listar os carros
+def listar_carros(request):
+    carros = Carro.objects.all()
+    return render(request, 'carros/listar_carros.html', {'carros': carros})
+
+# view que vai exibir detalhes de um carro especifico
+def detalhes_carro(request, carro_id):
+    carro = get_object_or_404(Carro, id=carro_id)
+    return render(request, 'carros/detalhes_carro.html', {'carro': carro})
+
+# view para cadastrar um novo carro
+def cadastrar_carro(request):
+    if request.method == 'POST':
+        form = CarroForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('carros:listar_carros')
+    else:
+        form = CarroForm()
+    return render(request, 'carros/cadastrar_carro.html', {'form': form})
